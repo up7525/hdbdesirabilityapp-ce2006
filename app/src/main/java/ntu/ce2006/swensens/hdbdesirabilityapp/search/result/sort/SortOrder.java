57f9d0b91ce6b37f7fae5770ae0f61f29de0e7a3
jonathanliem94 <jonathanliem94@gmail.com>
2017-04-10 10:39:45 +0800

    Code Cleanup

--- app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/result/sort/SortOrder.java
+++
@@ -1,9 +0,0 @@
-package ntu.ce2006.swensens.hdbdesirabilityapp.search.result.sort;
-
-/**
- * Created by trollpc on 24/03/17.
- */
-
-public enum SortOrder {
-    PRICE_ASCENDING, PRICE_DESCENDING, DEFAULT;
-}
