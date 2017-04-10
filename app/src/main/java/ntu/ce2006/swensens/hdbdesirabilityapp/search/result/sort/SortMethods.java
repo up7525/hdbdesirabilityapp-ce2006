878e9077cea4e27f0a61b633c07e2fc89a4809d0
jonathanliem94 <jonathanliem94@gmail.com>
2017-04-10 12:20:39 +0800

    Fix issue regarding missing sort

---
+++ app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/result/sort/SortMethods.java
@@ -0,0 +1,18 @@
+package ntu.ce2006.swensens.hdbdesirabilityapp.search.result.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by trollpc on 20/03/17.
+ */
+
+public class SortMethods{
+
+    public SortMethods() {
+
+    }
+
+
+}
