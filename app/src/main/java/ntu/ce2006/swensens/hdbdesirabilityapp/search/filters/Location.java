a89126763006c04c57eb2451672f3963f1d83cd9
jonathanliem94 <jonathanliem94@gmail.com>
2017-04-13 23:00:19 +0800

    Javadoc comments

--- app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/filters/Location.java
+++ app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/filters/Location.java
@@ -1,6 +1,7 @@
 package ntu.ce2006.swensens.hdbdesirabilityapp.search.filters;
 
 /**
+ * An emuneration class to define Town areas
  * @author Jonathan
  */
 
@@ -14,11 +15,19 @@ public enum Location {
     SERANGOON("SERANGOON"), TAMPINES("TAMPINES"), TOA_PAYOH("TOA PAYOH"), WOODLANDS("WOODLANDS"),
     YISHUN("YISHUN");
 
+    /**
+     * Location enum string
+     */
     private String name;
 
     Location(String name) {
         this.name = name;
     }
+
+    /**
+     * A simple string getter for the enum
+     * @return string representation of the enum
+     */
     @Override
     public String toString() {
         return name;
--- app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/filters/Size.java
+++ app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/filters/Size.java
@@ -1,17 +1,24 @@
 package ntu.ce2006.swensens.hdbdesirabilityapp.search.filters;
 
 /**
+ * An emuneration class to define Flat sizes
  * @author Jonathan
  */
 
 public enum Size {
     ROOM_2("2 ROOM"), ROOM_3("3 ROOM"), ROOM_4("4 ROOM"), ROOM_5("5 ROOM"), EXECUTIVE("EXECUTIVE");
-
+    /**
+     * Size enum string
+     */
     private String size;
     Size(String size) {
         this.size = size;
     }
 
+    /**
+     * A simple string getter for the enum
+     * @return string representation of the enum
+     */
     @Override
     public String toString() {
         return size;
