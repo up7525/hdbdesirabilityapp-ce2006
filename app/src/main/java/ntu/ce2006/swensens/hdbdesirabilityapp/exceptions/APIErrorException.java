9e4c753e029c76db36c2ac996e4b025d80baeb06
jonathanliem94 <jonathanliem94@gmail.com>
2017-04-13 23:13:09 +0800

    Javadoc comments

--- app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/exceptions/APIErrorException.java
+++ app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/exceptions/APIErrorException.java
@@ -1,6 +1,6 @@
 package ntu.ce2006.swensens.hdbdesirabilityapp.exceptions;
 
-/**
+/** Generic Error Message when communication to the API has failed
  * @author Swensens
  * Created by Swensens on 20/03/17.
  */
