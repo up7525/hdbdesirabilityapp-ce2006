2aa6d6e48872926f7b2997ec53fdfc6f9683221f
jonathanliem94 <jonathanliem94@gmail.com>
2017-04-13 23:01:06 +0800

    Javadoc comments

--- app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/query/Query.java
+++ app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/query/Query.java
@@ -1,8 +1,5 @@
 package ntu.ce2006.swensens.hdbdesirabilityapp.search.query;
 
-/**
- * Created by Jonathan on 29-Mar-17.
- */
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,101 +23,154 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * Query in builder pattern
+/**Query in builder pattern
  *
  * Example Construction
  * Query query = new Query.Builder().locations(locationList).sizes(sizesList).price(0, 100)
  *          .areas(0,4).amenities(amenitiesList).build();
- *
- * @author Wang Chun-Yin
- * Created by Swensens on 20/03/17.
+ * @author Jonathan
+ * Created by Jonathan on 29-Mar-17.
  */
 
-public class Query implements Serializable{
 
+public class Query implements Serializable{
+    /**
+     * Unique id to identify the query object
+     */
     @SerializedName("id")
     private int id_key;
 
-    //String Identifier
+    /**
+     * String identifier for the query
+     */
     @SerializedName("desc")
     private String desc;
 
-    // A list of location filters
+    /**
+     * Location lists as chosen by user
+     */
     @SerializedName("locations")
     private List<Location> locationFilters;
 
-    // A list of size filters
+    /**
+     * Size lists as chosen by user
+     */
     @SerializedName("size")
     private List<Size> sizeFilters;
 
-    // Price range: [minimum, maximum]
+    /**
+     * Price range as entered in by user
+     */
     @SerializedName("price")
     private int[] priceFilters;
 
-    // A list of amenity filters
+    /**
+     * Amenities list as chosen by user
+     */
     @SerializedName("amen")
     private List<Amenities> amenitiesFilters;
 
     /**
-     * Builder Class
+     * Builder Class for Query object
      */
     public static class Builder {
-        // Optional Parameters
+        /**
+         * Default value of id
+         */
         private int id_key = 0;
 
-        //String identifier
+        /**
+         * Default value of string descriptor
+         */
         private String desc = "Default";
 
-        // A list of location filters
+        /**
+         * Default location list
+         */
         private List<Location> locationFilters = new ArrayList<>();
 
-        // A list of size filters
+        /**
+         * Default size list
+         */
         private List<Size> sizeFilters = new ArrayList<>();
 
-        // Price range: [minimum, maximum]
+        /**
+         * Default price range
+         */
         private int[] priceFilters = new int[2];
 
-        // A list of amenity filters
+        /**
+         * Default amenity list
+         */
         private List<Amenities> amenitiesFilters = new ArrayList<>();
 
+        /**
+         * Setting id parameter for builder
+         * @param id id of query object to be built
+         */
         public Builder idDB(int id){
             id_key = id;
             return this;
         }
 
+        /**
+         * Setting string descriptor parameter for builder
+         * @param desc desc of query object to be built
+         */
         public Builder desc(String desc){
             desc = desc;
             return this;
         }
 
+        /**
+         * Setting location list parameter for builder
+         * @param locations location list of query object to be built
+         */
         public Builder locations(ArrayList<Location> locations) {
             locationFilters = locations;
             return this;
         }
 
+        /**
+         * Setting size list parameter for builder
+         * @param sizes size list of query object to be built
+         */
         public Builder size(ArrayList<Size> sizes) {
             sizeFilters = sizes;
             return this;
         }
 
+        /**
+         * Setting price range parameter for builder
+         * @param arr price range of query object to be built
+         */
         public Builder price(int[] arr) {
             priceFilters[0] = arr[0];
             priceFilters[1] = arr[1];
             return this;
         }
 
+        /**
+         * Setting amenities list parameter for builder
+         * @param amenities amenities of query object to be built
+         */
         public Builder amenities(ArrayList<Amenities> amenities) {
             amenitiesFilters = amenities;
             return this;
         }
 
+        /**
+         * Method to build Query object
+         * @return Query object built
+         */
         public Query build() {
             return new Query(this);
         }
     }
 
+    /**
+     * Constructor for the Query object (private)
+     */
     private Query(Builder builder) {
         id_key = builder.id_key;
         desc = builder.desc;
@@ -130,10 +180,18 @@ public class Query implements Serializable{
         amenitiesFilters = builder.amenitiesFilters;
     }
 
+    /**
+     * Returns the id
+     * @return id of query object
+     */
     public int getId_key() {
         return id_key;
     }
 
+    /**
+     * Returns the string descriptor
+     * @return string descriptor
+     */
     public String getDesc(){
         desc = "";
         if(getLocationFilters().size() > 0)
@@ -146,34 +204,66 @@ public class Query implements Serializable{
         return desc;
     }
 
+    /**
+     * Returns the location filter list
+     * @return location filter list
+     */
     public List<Location> getLocationFilters() {
         return locationFilters;
     }
 
+    /**
+     * Returns the size filter list
+     * @return size filter list
+     */
     public List<Size> getSizeFilters() {
         return sizeFilters;
     }
 
+    /**
+     * Returns the price filter
+     * @return price filter
+     */
     public int[] getPriceFilters() {
         return priceFilters;
     }
 
+    /**
+     * Returns the amenities filter
+     * @return amenities filter
+     */
     public List<Amenities> getAmenitiesFilters() {
         return amenitiesFilters;
     }
 
+    /**
+     * Returns a value if the location filter is empty
+     * @return 0 if location filter is empty
+     */
     public boolean isLocationEmpty() {
         return locationFilters.size() == 0;
     }
 
+    /**
+     * Returns a value if size filter is empty
+     * @return 0 if size filter is empty
+     */
     public boolean isSizeEmpty() {
         return locationFilters.size() == 0;
     }
 
+    /**
+     * Returns a value if price filter is empty
+     * @return 0 if price filter is empty
+     */
     public boolean isPriceEmpty() {
         return priceFilters[0] == 0 && priceFilters[1] == 0;
     }
 
+    /**
+     * Returns a value if amenities filter is empty
+     * @return 0 if amenities filter is empty
+     */
     public boolean isAmenitiesEmpty() {
         return amenitiesFilters.size() == 0;
     }
