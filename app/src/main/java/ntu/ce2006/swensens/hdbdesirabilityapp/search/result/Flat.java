e2efeb6cb8b1ac6f30678234faa7639ead920bb6
jonathanliem94 <jonathanliem94@gmail.com>
2017-04-13 23:02:36 +0800

    Javadoc comments

--- app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/result/Flat.java
+++ app/src/main/java/ntu/ce2006/swensens/hdbdesirabilityapp/search/result/Flat.java
@@ -4,36 +4,63 @@ import java.io.*;
 import java.util.*;
 import ntu.ce2006.swensens.hdbdesirabilityapp.search.result.sort.SortOrder;
 
-/**
- * @author Swensens
+/** Flat in builder pattern
+ * @author Wang Chun-Yin
  * Created by Swensens on 20/03/17.
  */
 
 public class Flat implements Serializable, Comparable<Flat> {
 
-    // Score of the result
+    /**
+     * Desirability Score of the Flat
+     */
     private double score;
 
-    // Address of the result
+    /**
+     * String representation of the full address
+     */
     private String address;
 
+    /**
+     * String representation of the street name
+     */
     private String streetName;
 
+    /**
+     * The block of the Flat
+     */
     private String block;
 
+    /**
+     * Town area where the Flat is located
+     * @see ntu.ce2006.swensens.hdbdesirabilityapp.search.filters.Location
+     */
     private String town;
 
+    /**
+     * Size of the Flat
+     * @see ntu.ce2006.swensens.hdbdesirabilityapp.search.filters.Size
+     */
     private String size;
 
-    // Price of the result
+    /**
+     * Price for which the Flat was sold for
+     */
     private double price;
 
-    // Area of the result
+    /**
+     * Size of the flat in square metres
+     */
     private double area;
 
-    // Amenities of the result in format {name, distance}
+    /**
+     * Amenities of the result in format {name, distance}
+     */
     private HashMap<String, Integer> amenities;
 
+    /**
+     * Defines for the Builder class for Flat Object
+     */
     public static class Builder {
         // Required Parameters
         private double score = 0;
@@ -45,9 +72,21 @@ public class Flat implements Serializable, Comparable<Flat> {
         private double price;
         private double area;
 
-        // Optional Parameters
+        /**
+         * Default Amenities is null.
+         */
         private HashMap<String, Integer> amenities = new HashMap<>();
 
+        /**
+         * Constructor for the Builder class of the Flat object
+         * @param block Block of Flat sold
+         * @param streetName Street Name of Flat sold
+         * @param town Town area of Flat sold
+         * @param address String representation of address
+         * @param size Size of Flat (in terms of rooms)
+         * @param price Price of flat sold
+         * @param area Area of flat in square meters
+         */
         public Builder(String block, String streetName, String town, String address, String size, double price, double area) {
             this.block = block;
             this.streetName = streetName;
@@ -57,25 +96,38 @@ public class Flat implements Serializable, Comparable<Flat> {
             this.price = price;
             this.area = area;
         }
-        
-        /*
-        public Builder amenities(String Name)
-        */
+
+        /**
+         * Set Amenities parameter in soon-to-be generated Flat object
+         * @param amenities A hashmap of Amenities as a filter
+         */
         public Builder amenities(HashMap<String, Integer> amenities) {
             this.amenities = amenities;
             return this;
         }
 
+        /**
+         * Set Score parameter in soon-to-be generated Flat object
+         * @param score
+         */
         public Builder score(double score) {
             this.score = score;
             return this;
         }
 
+        /**
+         * Function to construct the Flat object
+         * @return The constructed Flat object
+         */
         public Flat build() {
             return new Flat(this);
         }
     }
 
+    /**
+     * Constructor for the Flat object
+     * @param builder Builder class
+     */
     private Flat(Builder builder) {
         score = builder.score;
         block = builder.block;
@@ -88,29 +140,48 @@ public class Flat implements Serializable, Comparable<Flat> {
         amenities = builder.amenities;
     }
 
+    /**
+     * Getter for block value
+     * @return Block of Flat
+     */
     public String getBlock() {
         return block;
     }
 
+    /**
+     * Getter for Streetname
+     * @return Street Name of where Flat is situated
+     */
     public String getStreetName() {
         return streetName;
     }
 
+    /**
+     * Getter for Town where Flat is located in
+     * @return Town
+     */
     public String getTown() {
         return town;
     }
 
+    /**
+     * Setter for amenities
+     * @param amenities
+     */
     public void setAmenities(HashMap<String, Integer> amenities) {
         this.amenities = amenities;
     }
 
+    /**
+     * Setter for desirability score
+     * @param score
+     */
     public void setScore(double score) {
         this.score = score;
     }
 
     /**
      * Returns the score
-     *
      * @return score of the result
      */
     public double getScore() {
@@ -119,7 +190,6 @@ public class Flat implements Serializable, Comparable<Flat> {
 
     /**
      * Returns the address
-     *
      * @return address of the result
      */
     public String getAddress() {
@@ -128,7 +198,6 @@ public class Flat implements Serializable, Comparable<Flat> {
 
     /**
      * Returns the size of the flat
-     *
      * @return size of the flat
      */
     public String getSize() {
@@ -137,7 +206,6 @@ public class Flat implements Serializable, Comparable<Flat> {
 
     /**
      * Returns the price
-     *
      * @return price of the result
      */
     public double getPrice() {
@@ -146,7 +214,6 @@ public class Flat implements Serializable, Comparable<Flat> {
 
     /**
      * Returns the area
-     *
      * @return area of the result
      */
     public double getArea() {
@@ -155,13 +222,16 @@ public class Flat implements Serializable, Comparable<Flat> {
 
     /**
      * Returns the amenities in format {name, distance}
-     *
      * @return amenities of result in hashmap
      */
     public HashMap<String, Integer> getAmenities() {
         return amenities;
     }
 
+    /**
+     * Returns String format of Flat object for display on the application
+     * @return String representation of Flat object
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("Score: " + String.format("%.2f", score) + "/10" + System.getProperty("line.separator")
@@ -182,6 +252,11 @@ public class Flat implements Serializable, Comparable<Flat> {
         return sb.toString();
     }
 
+    /**
+     * Compare two Flat objects
+     * @param o Usually a Flat object so as to compare them
+     * @return true if both are equal, false if not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -199,6 +274,10 @@ public class Flat implements Serializable, Comparable<Flat> {
         return size.equals(flat.size);
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public int hashCode() {
         int result;
@@ -217,6 +296,11 @@ public class Flat implements Serializable, Comparable<Flat> {
         return result;
     }
 
+    /**
+     * Default sorting method, by desirability score
+     * @param otherFlat Other Flat object for comparison purposes
+     * @return 0 if same score, 1, if this object is smaller than the other, -1 if greater
+     */
     // DEFAULT (By Score) UNDERNEATH ARE SORTING
     // (TIP): Use Collections.sort(arrayList<Flat>, Flat.DefaultComparator());
     @Override
@@ -230,7 +314,12 @@ public class Flat implements Serializable, Comparable<Flat> {
         return 0;
     }
 
-    // Other sort order
+    /**
+     * Method to decide which sorting algorithmn to use
+     * @param otherFlat Other Flat object for comparison purposes
+     * @param sortOrder
+     * @return value depends on the sorting algorithmn.
+     */
     public int compareTo(Flat otherFlat, SortOrder sortOrder) {
         switch (sortOrder) {
             case PRICE_ASCENDING:
@@ -243,6 +332,11 @@ public class Flat implements Serializable, Comparable<Flat> {
         return 0;
     }
 
+    /**
+     * Sorting method by price, ascending
+     * @param otherFlat Other Flat object for comparison purposes
+     * @return 0 if same price, 1, if this Flat's price is larger than the other, -1 if smaller
+     */
     private int priceAscendingCompare(Flat otherFlat) {
         if (this.price > otherFlat.price) {
             return 1;
@@ -252,6 +346,11 @@ public class Flat implements Serializable, Comparable<Flat> {
         return 0;
     }
 
+    /**
+     * Sorting method by price, descending
+     * @param otherFlat Other Flat object for comparison purposes
+     * @return 0 if same price, -1, if this Flat's price is larger than the other, 1 if smaller
+     */
     private int priceDescendingCompare(Flat otherFlat) {
         if (this.price > otherFlat.price) {
             return -1;
